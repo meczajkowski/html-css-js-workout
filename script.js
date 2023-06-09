b6e79f6019ed5b155de8c432d71558d59d7de371
Michal <michal.erik.czajkowski@gmail.com>
2023-06-09 20:20:43 +0200

    feat: change box border color in time

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('Hello from Nerdbord!')
+const box = document.querySelector('.box');
+
+const colors = ['red', 'blue', 'green', 'black'];
+
+let i = 0;
+setInterval(() => {
+  box.style.borderColor = colors[i];
+  i = (i + 1) % colors.length;
+}, 3000);
